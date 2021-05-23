0295bc07dff8f20e3ab77e8f712e580b8a3b2008
ris46ithi <smohamedithiris@gmail.com>
2021-05-23 08:56:53 -0400

    day1 codeupdate

---
+++ day1.py
@@ -0,0 +1,28 @@
+import sys
+#day print values
+print ("Hello World")
+name = "hello","world"
+type (name) #tuples
+name1,name2 = "hello","world" 
+print(name1) #hello
+print(name2) #world
+################################
+###interpreter mode commands####
+a = 1+2+3
+# a = 1+2+3+ \ 
+#    4+5 ### "\ effect is 2 line"
+################################
+#####Id = value of variable and string ###
+print (a)
+id (a) 
+#####type ###
+type (a)
+#########
+sys.getsizeof(a)
+######input user date ######
+user_data = input("set value: ")
+print (user_data)
+type(user_data) ###user input data always be String#
+##########################
+#####4 Bit Values #######
+
